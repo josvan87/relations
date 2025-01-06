67c80860f471b3153a8673c69ba5fc2017055ca7
josvan87 <josvansouza@gmail.com.br>
2025-01-06 17:34:31 -0300

    mudança do método usando math

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::match(['get', 'post'], function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
